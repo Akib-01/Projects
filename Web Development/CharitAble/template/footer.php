72036f14d5028d913e96cb7a4436e29191ebb054
Akib-01 <99616323+Akib-01@users.noreply.github.com>
2022-05-05 22:51:36 +0600

    Add files via upload

---
+++ Web Development/CharitAble/template/footer.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="../views/CSS/style.css">
+</head>
+<body>
+	
+
+	<footer class="footer">
+		
+		<div class="footer-body">
+
+			<div class="footer-section">
+
+
+				<div class="footer-aboutus">
+					<h4 style="color:#f1f1f1;">About Us</h4>
+					<p style="color:#bab7b6;">CharitAble connects nonprofits, donors, and underprivileged. We work as a non-profit organization and also help other non-profit organizations so that they can serve their own communities. This way we can reach more people all over the world.
+
+					</p>		
+				</div>
+
+
+
+
+
+
+				<div class="list_links">
+				<h4 class="footer-section-title">Quick Links</h4>
+
+				<div class="footer-section-body">
+					
+					<ul class="list-links">
+						<li>
+							<a href="#">Events</a>
+						</li>
+						<li>
+							<a href="#">Projects</a>
+						</li>
+						<li>
+							<a href="#">Gallery</a>
+						</li>
+
+					</ul><!-- /.list-links -->
+
+					<ul class="list-links2">
+						<li>
+							<a href="#">Terms Of Use</a>
+						</li>
+						<li>
+							<a href="#">Legal Desclaimer</a>
+						</li>
+						<li>
+							<a href="#">Privacy Policy</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+		<div class="footer-contactus">
+			<h4 style="color:#f1f1f1;">Contact Us</h4>
+			<p style="color:#bab7b6;"">House #20 (3rd Floor) Road # 17, Nikanjia-2 Dhaka, Bangladesh <br> Telephone: +88-01-2233-3345 <br> Email: CharitAble@charity.org.</p>
+		</div>
+
+		<div class="copyright">
+			<p>©2022-2022 Copyright. All rights reserved by CharitAble.</p>
+		</div>
+
+	</footer>
+
+</body>
+</html>
+
+
+
