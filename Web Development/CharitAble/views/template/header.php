7b0b8dcab13c612cdb3e13c1e9f84945332d11c0
Akib-01 <99616323+Akib-01@users.noreply.github.com>
2022-05-05 22:58:09 +0600

    Add files via upload

---
+++ Web Development/CharitAble/views/template/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Header</title>
+	<link rel="stylesheet" href="../views/CSS/style.css">	
+</head>
+<body>
+	<div class="sticky_header">
+	<header class="header" id="myheader">
+		<div class="sheader">
+			<div class="header-logo">
+			<a href="../views/Dashboard.php">
+				<img src="../views/CSS/assets/logo1.png" alt="CharitAble | Foundation" class="header-img">
+				<span class="site-description">Foundation</span>
+			</a>
+			</div>
+			<nav class="nav">
+				<ul>
+					<li class="hh">
+						<a href="../views/Dashboard.php">Home</a>
+					</li>
+					<li class="hh">
+						<a href="#">About Us</a>
+						
+					</li>
+					<li class="dropdown">
+						<a href="../views/Dashboard.php#events" class="dropbtn">Events</a>
+						<ul>
+							<div class="dropdown-content">
+								<a href="">Single Event</a>
+							</div>
+						</ul>
+						
+					</li>
+					<li class="hh">
+						<a href="#" >Contact Us</a>
+					</li>
+					<li>
+						<a href="../views/DonateMoney.php" class="donate">Donate</a>
+					</li>
+
+				</ul>
+				
+			</nav>
+
+		</div>
+		
+	</header>
+	</div>
+</body>
+</html>
